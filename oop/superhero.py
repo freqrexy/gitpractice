a60fa8862e680758957dce660a88060b25e87cf1
freqrexy <bev.wooff@gmail.com>
2024-12-09 14:24:07 +0000

    Added more class setters and getters

--- oop/superhero.py
+++ oop/superhero.py
@@ -1,11 +1,15 @@
 class Superhero():
-    def __init__(self, superhero_name, superhero_identity, superhero_power, superhero_archenemy):
+    def __init__(self, superhero_name, superhero_identity, superhero_power, superhero_archenemy, superhero_transform):
         self.name = superhero_name
         self.identity = superhero_identity
         self.power = superhero_power
         self.enemy = superhero_archenemy
+        self.transform = superhero_transform
     
     def introduce(self):
         print(f"My normal name is {self.identity}, but I fight crime under the alias of {self.name}!")
         print(f"I {self.power}, an essential power to save the day.")
         print(f"I have fought many enemies including my arch nemesis, {self.enemy}.")
+
+    def transform(self):
+        print(f"{self.identity} {self.transform} and became {self.name}!")
--- oop/superheromain.py
+++ oop/superheromain.py
@@ -1,9 +1,14 @@
 from superhero import Superhero
     
-hero1 = Superhero ("Mermaid Man","Ernie Huckler","can breathe underwater","The Dirty Bubble")
-hero2 = Superhero ("Bananaman","Eric Wimp","have superhuman strength","General Blight")
-hero3 = Superhero ("Radioactive Man","Claude Kane III","can absorb radiation","Annihilus")
+hero1 = Superhero ("Mermaid Man","Ernie Huckler","can breathe underwater","The Dirty Bubble","screamed 'EVIL!'")
+hero2 = Superhero ("Bananaman","Eric Wimp","have superhuman strength","General Blight","ate a banana")
+hero3 = Superhero ("Radioactive Man","Claude Kane III","can absorb radiation","Annihilus","said 'Up and Atom!'")
+hero4 = Superhero ("Freakazoid","Dexter Douglas","know everything about the Internet","The Lobe","shouted 'Freak Out!'")
 
 hero1.introduce()
-hero2.introduce()
-hero3.introduce()
+#hero2.introduce()
+#hero3.introduce()
+
+hero1.set_lair("Mermalair")
+
+hero1.transform()
